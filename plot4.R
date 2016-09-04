4e60271dc773ecfdca99ceca2afad1b2c12bb8b7
Tiago Henriques <balgan@balgan.eu>
2016-09-04 02:05:04 +0200

    Fixes code
    code fix for box

--- plot4.R
+++ plot4.R
@@ -21,10 +21,10 @@ plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power",
 
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(datetime, subMeter1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMeter2, type="l", col="red")
+lines(datetime, subMeter3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="n")
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 
